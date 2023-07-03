8e4ec4062391103f5c2195d9146e69faf8e260be
AmitReuveni <amitreuveni@gmail.com>
2023-07-03 16:59:35 +0300

    Added store income button and method

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -8,17 +8,38 @@ public class Store : MonoBehaviour
 
     private float baseStoreCost;
     private int storeCount;
+    private float baseStoreProfit;
     [SerializeField] private TMP_Text storeCountText;
 
     [SerializeField] private TMP_Text currentBalanceText;
+
+    private float incomeTimer = 4f;
+    private float currentIncomeTime = 0;
+    private bool startTimer;
 
     private void Start()
     {
         storeCount = 1;
         currentBalance = 2;
         baseStoreCost = 1.5f;
+        baseStoreProfit = 0.5f;
         storeCountText.text = storeCount.ToString();
-        UpdateCurrentBalance(currentBalance);
+        UpdateCurrentBalanceUI();
+    }
+
+    private void Update()
+    {
+        if (startTimer)
+        {
+            currentIncomeTime += Time.deltaTime;
+            if (currentIncomeTime > incomeTimer)
+            {
+                startTimer = false;
+                currentIncomeTime = 0f;
+                currentBalance += baseStoreProfit * storeCount;
+                UpdateCurrentBalanceUI();
+            }
+        }
     }
 
     public void BuyStoreOnClick()
@@ -27,14 +48,22 @@ public class Store : MonoBehaviour
         if (currentBalance < baseStoreCost) return;
 
         currentBalance -= baseStoreCost;
-        UpdateCurrentBalance(currentBalance);
+        UpdateCurrentBalanceUI();
 
         storeCount++;
         storeCountText.text = storeCount.ToString();
     }
 
-    private void UpdateCurrentBalance(float newBalance)
+    private void UpdateCurrentBalanceUI()
     {
-        currentBalanceText.text = "$" + $"{newBalance:0.00}";
+        currentBalanceText.text = "$" + $"{currentBalance:0.00}";
+    }
+
+    public void StoreOnClick()
+    {
+        if (startTimer) return;
+        
+        startTimer = true;
+        currentIncomeTime = 0f;
     }
 }
