005b62d718b7267cf55ae0ecb52906d2b82ecdc2
AmitReuveni <amitreuveni@gmail.com>
2023-07-03 16:43:56 +0300

    Added current balance tracking

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -1,20 +1,40 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class Store : MonoBehaviour
 {
+    private float currentBalance;
+
+    private float baseStoreCost;
     private int storeCount;
     [SerializeField] private TMP_Text storeCountText;
+
+    [SerializeField] private TMP_Text currentBalanceText;
 
     private void Start()
     {
         storeCount = 1;
+        currentBalance = 2;
+        baseStoreCost = 1.5f;
         storeCountText.text = storeCount.ToString();
+        UpdateCurrentBalance(currentBalance);
     }
 
     public void BuyStoreOnClick()
     {
+        Debug.Log(currentBalance);
+        if (currentBalance < baseStoreCost) return;
+
+        currentBalance -= baseStoreCost;
+        UpdateCurrentBalance(currentBalance);
+
         storeCount++;
         storeCountText.text = storeCount.ToString();
+    }
+
+    private void UpdateCurrentBalance(float newBalance)
+    {
+        currentBalanceText.text = "$" + $"{newBalance:0.00}";
     }
 }
