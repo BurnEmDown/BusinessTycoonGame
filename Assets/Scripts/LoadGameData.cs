654fa26adf5cda48a27565d73163a52c651cec6c
AmitReuveni <amitreuveni@gmail.com>
2023-08-28 23:44:16 +0300

    slight refactoring to loadgamedata script

--- Assets/Scripts/LoadGameData.cs
+++ Assets/Scripts/LoadGameData.cs
@@ -29,46 +29,57 @@ public class LoadGameData : MonoBehaviour
 
         foreach (XmlNode storeInfo in StoreList)
         {
-            GameObject newStore = Instantiate(storePrefab);
-
-            StoreController storeObj = newStore.GetComponent<StoreController>();
-            StoreData data = new();
-            
-            XmlNodeList storeNodes = storeInfo.ChildNodes;
-            foreach (XmlNode storeNode in storeNodes)
-            {
-                Debug.Log(storeNode.Name);
-                Debug.Log(storeNode.InnerText);
-                switch (storeNode.Name)
-                {
-                    case "StoreName":
-                        data.storeName = storeNode.InnerText;
-                        break;
-                    case "BaseStoreProfit":
-                        data.baseStoreProfit = float.Parse(storeNode.InnerText);
-                        break;
-                    case "BaseStoreCost":
-                        data.baseStoreCost = float.Parse(storeNode.InnerText);
-                        break;
-                    case "StoreCount":
-                        data.storeCount = int.Parse(storeNode.InnerText);
-                        break;
-                    case "IncomeTimer":
-                        data.incomeTime = float.Parse(storeNode.InnerText);
-                        break;
-                    case "StoreMultiplier":
-                        data.storeMultiplier = float.Parse(storeNode.InnerText);
-                        break;
-                    case "Image":
-                        Sprite newSprite = Resources.Load<Sprite>(storeNode.InnerText);
-                        data.image = newSprite;
-                        break;
-                }
-            }
-            
-            storeObj.transform.SetParent(storePanel.transform);
-            storeObj.Init(data);
+            LoadStore(storeInfo);
         }
+    }
+
+    private void LoadStore(XmlNode storeInfo)
+    {
+        GameObject newStore = Instantiate(storePrefab);
+
+        StoreController storeObj = newStore.GetComponent<StoreController>();
+            
+        XmlNodeList storeNodes = storeInfo.ChildNodes;
+        StoreData data = GetDataFromStoreNode(storeNodes);
+            
+        storeObj.transform.SetParent(storePanel.transform);
+        storeObj.Init(data);
+    }
+
+    private StoreData GetDataFromStoreNode(XmlNodeList storeNodes)
+    {
+        StoreData data = new();
+
+        foreach (XmlNode storeNode in storeNodes)
+        {
+            switch (storeNode.Name)
+            {
+                case "StoreName":
+                    data.storeName = storeNode.InnerText;
+                    break;
+                case "BaseStoreProfit":
+                    data.baseStoreProfit = float.Parse(storeNode.InnerText);
+                    break;
+                case "BaseStoreCost":
+                    data.baseStoreCost = float.Parse(storeNode.InnerText);
+                    break;
+                case "StoreCount":
+                    data.storeCount = int.Parse(storeNode.InnerText);
+                    break;
+                case "IncomeTimer":
+                    data.incomeTime = float.Parse(storeNode.InnerText);
+                    break;
+                case "StoreMultiplier":
+                    data.storeMultiplier = float.Parse(storeNode.InnerText);
+                    break;
+                case "Image":
+                    Sprite newSprite = Resources.Load<Sprite>(storeNode.InnerText);
+                    data.image = newSprite;
+                    break;
+            }
+        }
+        
+        return data;
     }
 
     private void SetCompanyName(XmlDocument xmlDocument)
